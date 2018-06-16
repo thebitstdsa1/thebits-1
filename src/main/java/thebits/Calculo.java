9a42f58b54f4b555133e7512738ca7927a8aa64a
Hruza Gabriel Carlos <gabriel.hruza@gmail.com>
2018-06-16 16:25:32 -0300

    Update Calculo.java

--- src/main/java/thebits/Calculo.java
+++ src/main/java/thebits/Calculo.java
@@ -5,4 +5,26 @@ public class Calculo {
 	public int multiplicar(int a, int b) {
 		return a*b;
 	}
+	
+	if(temp<10){
+            mensaje = "Hace mucho frío.";
+        }
+        else{
+            if(temp<15){
+                mensaje = "Hace poco frío.";
+            }
+            else{
+                if(temp<25){
+                    mensaje = "Hace una temperatura normal.";
+                }
+                else{
+                    if(temp<30){
+                        mensaje = "Hace poco calor.";
+                    }
+                    else{
+                        mensaje = "Hace mucho calor.";
+                    }
+                }
+            }
+	
 }

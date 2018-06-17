51564d0a456ed45985c533da907adaed63f8956d
Hruza Gabriel Carlos <gabriel.hruza@gmail.com>
2018-06-17 17:15:11 -0300

    Update Calculo.java

--- src/main/java/thebits/Calculo.java
+++ src/main/java/thebits/Calculo.java
@@ -5,10 +5,10 @@ import java.util.Calendar;
 public class Calculo {
 	
 	public int multiplicar(int a, int b) {
-		int c = a*b;
-		int d = a*b;
-		int e = a*b;
-		int f = a*b;
+		//int c = a*b;
+		//int d = a*b;
+		//int e = a*b;
+		//int f = a*b;
 		return a*b;
 	}
 }

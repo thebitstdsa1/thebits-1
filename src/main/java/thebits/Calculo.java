9f0a83192ebc7e8dcf3a39c24bc9078ea7739a1c
Hruza Gabriel Carlos <gabriel.hruza@gmail.com>
2018-06-21 21:58:00 -0300

    TDSA-1

--- src/main/java/thebits/Calculo.java
+++ src/main/java/thebits/Calculo.java
@@ -5,6 +5,10 @@ import java.util.Calendar;
 public class Calculo {
 	
 	public int multiplicar(int a, int b) {
+		//return a*b;
+		//return a*b;
+		//return a*b;//return a*b;
+		//return a*b;//return a*b;
 		return a*b;
 	}
 }
